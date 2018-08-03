f1670d19ba5093fb2e0c2829e1823be24fffd309
galustian <leonard@galustian.com>
2018-08-03 08:55:33 +0200

    finish homepage

--- server.py
+++ server.py
@@ -3,17 +3,14 @@ import matplotlib.pyplot as plt
 import mpld3
 from datetime import date, datetime
 import json
+import random
 from utils import read_log_only
+
 
 app = Flask(__name__)
 
+COLORS = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "black", "brown", "grey"]
 MAX_POINTS_IN_CHART = 180
-
-'''
-@app.route('/pie/<years>', methods=['GET', 'POST'])
-def pie_chart(years):
-    pass
-'''
 
 
 def total_days_of_difference(from_date, to_date):
@@ -65,7 +62,7 @@ def increment_date(from_day, from_month, from_year):
     return from_day, from_month, from_year
 
 
-def get_available_days_in_ordered_list(days, from_date, to_date):
+def get_available_dates_in_ordered_list(days, from_date, to_date):
     from_year = int(from_date[2])
     from_month = int(from_date[1])
     from_day = int(from_date[0])
@@ -95,24 +92,77 @@ def get_available_days_in_ordered_list(days, from_date, to_date):
     return days_list
 
 
-def get_n_days_avg(days, n_days_avg, from_date, to_date):
+def get_all_app_names():
+    app_names_list = []
+    with open('app_names.json') as f:
+        app_names_dict = json.load(f)
+    for key in app_names_dict:
+        app_names_list.append(app_names_dict[key])
+
+    return app_names_list
+
+
+def get_n_days_avg(days, n_days, from_date, to_date):
+    from_to_dates_list = get_available_dates_in_ordered_list(days, from_date, to_date)
+
+    app_names_list = get_all_app_names()
+
     avg_days = {}
+
+    # create avg_day and append to avg_days
+    for n in range(0, len(from_to_dates_list), n_days):
+        n_days_list = []
+
+        # append n_days to list
+        for i in range(n_days):
+            day = days[from_to_dates_list[n+i]]
+
+            # if app not used that day, create its dict entry with 0
+            for app_name in app_names_list:
+                if app_name not in day:
+                    day[app_name] = 0
+
+            n_days_list.append(day)
+
+        # create average day
+        avg_day = {}
+        # init avg_day dict
+        for app_name in app_names_list:
+            avg_day[app_name] = 0
+
+        for i in range(n_days):
+            for app_name in app_names_list:
+                avg_day[app_name] += n_days_list[i][app_name] / n_days
+
+        avg_days[from_to_dates_list[n+n_days]] = avg_day
+
+    return avg_days
+
+
+'''
+@app.route('/pie/<years>', methods=['GET', 'POST'])
+def pie_chart(years):
+    pass
+'''
+
+
+def create_date_obj(date_str):
+    date = date_str.split('-')
+    return date(int(date[2]), int(date[1]), int(date[0]))
 
 
 # format: 21_01_2018-16_09_2018
 @app.route('/<date_range>', methods=['GET', 'POST'])
 def homepage(date_range):
     dates = date_range.split('-')
-
     from_date = dates[0].split('_')
     to_date = dates[1].split('_')
     
     total_days = total_days_of_difference(from_date, to_date)
     n_days_avg_per_point = round(total_days / MAX_POINTS_IN_CHART)
 
-    years_list = [y for y in range(int(from_date[2]), int(to_date[2]) + 1)]
-    
     logs = {}
+    years_list = [y for y in range(int(from_date[2]), int(to_date[2]) + 1)]
     for y in years_list:
         logs[y] = read_log_only(year=y)
     
@@ -120,15 +170,20 @@ def homepage(date_range):
     days_avg = get_n_days_avg(days, n_days_avg_per_point, from_date, to_date)
 
     fig = plt.figure()
-    '''
-    da1 = date(2001, 2, 24)
-    da2 = date(2001, 3, 25)
-    da3 = date(2001, 4, 27)
-  
-    plt.plot_date([da1, da2, da3], [1, 3, 6], color='green', label='Terminal', linestyle='-')
-    plt.plot_date([da1, da2, da3], [4, 1, 0], color='red', label='Visual Studio Code', linestyle='-')
-    '''
-    
+
+    app_names_list = get_all_app_names()
+
+    for app_name in app_names_list:
+
+        date_obj_list = []
+        hours_list = []
+
+        for date_str in days_avg:
+            date_obj_list.append(create_date_obj(date_str))
+            hours_list.append(days_avg[date_str][app_name])
+
+        plt.plot_date(date_obj_list, hours_list, color=random.choice(COLORS), label=app_name, linestyle='-')
+
     plt.xlabel('Date')
     plt.ylabel('Hours')
     plt.legend()
