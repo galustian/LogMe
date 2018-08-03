d5d92395c1e813e4ef839be7c680b8179ac8f315
galustian <leonard@galustian.com>
2018-08-03 16:50:17 +0200

    bug fixes and many improvements

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, abort
 import matplotlib.pyplot as plt
 import mpld3
+import os
 import datetime
 import json
 import random
@@ -18,31 +19,36 @@ def number_of_logged_days(from_date, to_date):
 
 
 def get_days_dict(from_date, to_date):
-    from_month = int(from_date[1])
-    from_day = int(from_date[0])
-    
-    to_month = int(to_date[1])
-    to_day = int(to_date[0])
-
     logs = get_years_logs(int(from_date[2]), int(to_date[2]))
 
     days = {}
 
     # logs[log_y] is the log of an entire year to be partially included
     for log_y in logs:
-        for log_d in logs[log_y]:
-            log_date = log_d.split("-")
-            log_day = int(log_date[0])
-            log_month = int(log_date[1])
+        for log_date in logs[log_y]:
+            from_year = int(from_date[2])
+            from_month = int(from_date[1])
+            from_day = int(from_date[0])
+    
+            to_year = int(to_date[2])
+            to_month = int(to_date[1])
+            to_day = int(to_date[0])
 
-            if from_month < log_month < to_month:
-                days[log_d] = logs[log_y][log_d]
-            elif log_month == from_month:
+            log_day = int(log_date.split("-")[0])
+            log_month = int(log_date.split("-")[1])
+            
+            if log_y > from_year:
+                from_day = 1
+                from_month = 1
+
+            if from_month < log_month < to_month and log_y <= to_year:
+                days[log_date] = logs[log_y][log_date]
+            elif log_month == from_month and log_y <= to_year:
                 if log_day >= from_day:
-                    days[log_d] = logs[log_y][log_d]
-            elif log_month == to_month:
+                    days[log_date] = logs[log_y][log_date]
+            elif log_month == to_month and log_y <= to_year:
                 if log_day <= to_day:
-                    days[log_d] = logs[log_y][log_d]
+                    days[log_date] = logs[log_y][log_date]
     
     return days
 
@@ -50,13 +56,14 @@ def get_days_dict(from_date, to_date):
 def increment_date(from_day, from_month, from_year):
     if from_day < 31:
         from_day += 1
-    elif from_month < 12:
-        from_month += 1
-        from_day = 1
     else:
-        from_year += 1
+        from_day = 1
+        from_month += 1
+    
+    if from_month > 12:
         from_month = 1
         from_day = 1
+        from_year += 1
     
     return from_day, from_month, from_year
 
@@ -118,7 +125,6 @@ def get_app_colors_dict():
 
 def get_n_days_avg(days, n_days, from_date, to_date):
     from_to_dates_list = get_available_dates_in_ordered_list(days, from_date, to_date)
-    print(from_to_dates_list)
     app_names_list = get_all_app_names()
 
     avg_days = {}
@@ -126,7 +132,6 @@ def get_n_days_avg(days, n_days, from_date, to_date):
     # create avg_day and append to avg_days
     for n in range(0, len(from_to_dates_list), n_days):
         n_days_list = []
-
         # remaining days smaller than n_days (for the average)
         remaining_days = False
         if len(from_to_dates_list) - n < n_days:
@@ -136,7 +141,6 @@ def get_n_days_avg(days, n_days, from_date, to_date):
         # append n_days to list (for average)
         for i in range(n_days):
             day = days[from_to_dates_list[n+i]]
-
             # if app not used that day, create its dict entry with 0
             for app_name in app_names_list:
                 if app_name not in day:
@@ -146,7 +150,6 @@ def get_n_days_avg(days, n_days, from_date, to_date):
 
         # create average day
         avg_day = {}
-        # init avg_day dict
         for app_name in app_names_list:
             avg_day[app_name] = 0
 
@@ -162,13 +165,6 @@ def get_n_days_avg(days, n_days, from_date, to_date):
     return avg_days
 
 
-'''
-@app.route('/pie/<years>', methods=['GET', 'POST'])
-def pie_chart(years):
-    pass
-'''
-
-
 def create_date_obj(date_str):
     date = date_str.split('-')
     return datetime.date(int(date[2]), int(date[1]), int(date[0]))
@@ -178,7 +174,8 @@ def get_years_logs(from_year, to_year):
     logs = {}
     years_list = [y for y in range(from_year, to_year + 1)]
     for y in years_list:
-        logs[y] = read_log_only(year=y)
+        if os.path.isfile('logs/log-{}.json'.format(y)):
+            logs[y] = read_log_only(year=y)
 
     return logs
 
@@ -194,7 +191,7 @@ def create_graph(days_avg):
         for date_str in days_avg:
             date_obj_list.append(create_date_obj(date_str))
             hours_list.append(days_avg[date_str][app_name] / 3600)
-        plt.plot_date(date_obj_list, hours_list, color=app_colors_dict[app_name], label=app_name, markeredgewidth=2, linestyle='-', lw=3)
+        plt.plot_date(date_obj_list, hours_list, color=app_colors_dict[app_name], label=app_name, markeredgewidth=0, linestyle='-', lw=3)
 
     plt.xlabel('Date')
     plt.ylabel('Hours')
@@ -206,16 +203,17 @@ def create_bar(days_avg):
     app_colors_dict = get_app_colors_dict()
 
     for i, app_name in enumerate(app_names_list):
-        date_obj_list = []
+        date_str_list = []
         hours_list = []
 
         for date_str in days_avg:
-            date_obj_list.append(create_date_obj(date_str))
+            date_str_list.append(date_str)
             hours_list.append(days_avg[date_str][app_name] / 3600)
 
-        plt.bar(date_obj_list, hours_list, color=app_colors_dict[app_name], label=app_name, width=0.8)
-        ax = plt.gca()
-        ax.xaxis_date()
+        plt.bar(range(len(date_str_list)), hours_list, color=app_colors_dict[app_name], label=app_name, width=0.4, align='center')
+        plt.xticks(range(len(date_str_list)), date_str_list)
+        # ax = plt.gca()
+        # ax.xaxis_date()
 
     plt.xlabel('Date')
     plt.ylabel('Hours')
@@ -245,26 +243,29 @@ def create_pie(days_avg):
     plt.pie(hours_sum_list, labels=app_names_list, colors=colors_list, autopct='%1.1f%%')
 
 
-# format: 21_01_2018-16_09_2018
-@app.route('/<date_range>/<max_points_in_chart>', methods=['GET', 'POST'])
-def homepage(date_range, max_points_in_chart):
-    dates = date_range.split('-')
-    if len(dates) != 2:
-        return abort(404)
-    try:
-        int(max_points_in_chart)
-    except ValueError:
-        return abort(404)
-
-    from_date = dates[0].split('_')
-    to_date = dates[1].split('_')
-
-    if int(max_points_in_chart) > 15:
-        max_points_in_chart = 15
+# date format: 21_01_2018
+@app.route('/', methods=['GET'])
+def homepage():
+    from_date = None
+    to_date = None
+    max_points_in_chart = None
     
+    if 'start-date' not in request.args or 'end-date' not in request.args or 'max-datapoints' not in request.args:
+        now = datetime.datetime.now()
+        from_date = [now.day, now.month, now.year-1]
+        to_date = [now.day, now.month, now.year]
+        max_points_in_chart = MAX_POINTS_IN_BAR_CHART * 10
+    else:
+        from_date = request.args['start-date'].split('_')
+        to_date = request.args['end-date'].split('_')
+        max_points_in_chart = int(request.args['max-datapoints'])
+        # Validate date
+        if len(from_date) != 3 or len(to_date) != 3:
+            abort(404)
+      
     # For Graph and Bar-Chart, set average days per point
     total_days = number_of_logged_days(from_date, to_date)
-    n_days_avg_per_point = round(total_days / int(max_points_in_chart))
+    n_days_avg_per_point = round(total_days / max_points_in_chart)
     if n_days_avg_per_point == 0:
         n_days_avg_per_point = 1
 
@@ -278,22 +279,28 @@ def homepage(date_range, max_points_in_chart):
     create_pie(days_avg)
 
     bar_fig = plt.figure(figsize=(12, 8))
-    # Set at most 15 bars
-    if int(max_points_in_chart) > MAX_POINTS_IN_BAR_CHART:
-        max_points_in_chart = MAX_POINTS_IN_BAR_CHART
-
-        n_days_avg_per_point = round(total_days / int(max_points_in_chart))
+    # Set at most 15 bars START
+    if max_points_in_chart > MAX_POINTS_IN_BAR_CHART:
+        n_days_avg_per_point = round(total_days / MAX_POINTS_IN_BAR_CHART)
         if n_days_avg_per_point == 0:
             n_days_avg_per_point = 1
         days_avg = get_n_days_avg(days, n_days_avg_per_point, from_date, to_date)
-
+    # Set at most 15 bars END
     create_bar(days_avg)
 
     graph = mpld3.fig_to_html(graph_fig)
-    bar = mpld3.fig_to_html(bar_fig)
     pie = mpld3.fig_to_html(pie_fig)
+    bar = mpld3.fig_to_html(bar_fig)
 
-    return render_template('index.html', html_graph=graph, html_bar=bar, html_pie=pie)
+    template_args = {
+        'html_graph': graph,
+        'html_pie': pie,
+        'html_bar': bar,
+        'start_date': '_'.join([str(d) for d in from_date]),
+        'end_date': '_'.join([str(d) for d in to_date]),
+        'max_points': max_points_in_chart
+    }
+    return render_template('index.html', **template_args)
 
 
 if __name__ == '__main__':
